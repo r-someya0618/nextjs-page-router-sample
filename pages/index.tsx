eb22affd834a072fbb8c91634d045dede4106060
RyosukeSomeya <ryosuke.someya.0618.dev@gmail.com>
2023-10-05 00:03:32 +0900

    getStaticPropsの実装

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,35 @@
 import { Client } from '@notionhq/client';
-import { GetStaticProps } from 'next';
+import { GetStaticProps, NextPage } from 'next';
 
 const notion = new Client({
   auth: process.env.NOTION_TOKEN,
 });
 
-export const getStaticProps: GetStaticProps<{}> = async () => {
+export type Content =
+  | {
+      type: 'paragraph' | 'quote' | 'heading_2' | 'heading_3';
+      text: string | null;
+    }
+  | {
+      type: 'code';
+      text: string | null;
+      language: string | null;
+    };
+
+export type Post = {
+  id: string;
+  title: string | null;
+  slug: string | null;
+  createdTs: string | null;
+  lastEditedTs: string | null;
+  contents: Content[];
+};
+
+type StaticProps = {
+  post: Post | null;
+};
+
+export const getStaticProps: GetStaticProps<StaticProps> = async () => {
   const database = await notion.databases.query({
     database_id: process.env.NOTION_DATABASE_ID || '',
     filter: {
@@ -25,20 +49,103 @@ export const getStaticProps: GetStaticProps<{}> = async () => {
       },
     ],
   });
-  // console.dir(database, { depth: null });
+
+  const page = database.results[0];
+  // pageがない場合
+  if (!page) {
+    return {
+      props: {
+        post: null,
+      },
+    };
+  }
+  // propertiesがない（PartialPageObjectResponse型の場合）
+  if (!('properties' in page)) {
+    return {
+      props: {
+        post: {
+          id: page.id,
+          title: null,
+          slug: null,
+          createdTs: null,
+          lastEditedTs: null,
+          contents: [],
+        },
+      },
+    };
+  }
+  // pageはPageObjectResponse
+  let title: string | null = null;
+  if (page.properties['Name'].type === 'title') {
+    title = page.properties['Name'].title[0]?.plain_text ?? null;
+  }
+  let slug: string | null = null;
+  if (page.properties['Slug'].type === 'rich_text') {
+    slug = page.properties['Slug'].rich_text[0]?.plain_text ?? null;
+  }
 
   const blocks = await notion.blocks.children.list({
-    block_id: database.results[0]?.id,
+    block_id: page.id,
+  });
+  const contents: Content[] = [];
+
+  blocks.results.forEach((block) => {
+    if (!('type' in block)) {
+      return;
+    }
+    switch (block.type) {
+      case 'paragraph':
+        contents.push({
+          type: 'paragraph',
+          text: block.paragraph.rich_text[0]?.plain_text ?? null,
+        });
+        break;
+      case 'heading_2':
+        contents.push({
+          type: 'heading_2',
+          text: block.heading_2.rich_text[0]?.plain_text ?? null,
+        });
+        break;
+      case 'heading_3':
+        contents.push({
+          type: 'heading_3',
+          text: block.heading_3.rich_text[0]?.plain_text ?? null,
+        });
+        break;
+      case 'quote':
+        contents.push({
+          type: 'quote',
+          text: block.quote.rich_text[0]?.plain_text ?? null,
+        });
+        break;
+      case 'code':
+        contents.push({
+          type: 'code',
+          text: block.code.rich_text[0]?.plain_text ?? null,
+          language: block.code.language,
+        });
+        break;
+    }
   });
 
-  console.dir(blocks, { depth: null });
+  const post: Post = {
+    id: page.id,
+    title,
+    slug,
+    createdTs: page.created_time,
+    lastEditedTs: page.last_edited_time,
+    contents,
+  };
+
+  console.dir('post', post);
 
   return {
-    props: {},
+    props: { post },
   };
 };
 
-const Home = () => {
+const Home: NextPage<StaticProps> = ({ post }) => {
+  console.log(post);
   return <div></div>;
 };
 
